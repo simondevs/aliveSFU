2d852d0cd1ad151a08974a78faee6c45f38d5320
Gur Kohli <gurkohli@users.noreply.github.com>
2016-11-02 21:10:30 -0700

    Merged gagan's code with simondevs (#9)
    * - Added Exercise.swift
    
    * - Modified AddStrengthExercise.swift and Main.storyboard to implement SaveButton in Add Strength page
    
    * Cardio and strength.
    Added cancel and save functions for cardio and strength exercises. Also added Exercise.swift, a class for all exercises. -Gagan

--- AliveSFU/AliveSFU/AddCardioExercise.swift
+++ AliveSFU/AliveSFU/AddCardioExercise.swift
@@ -6,10 +6,34 @@
 //  Copyright © 2016 SimonDevs. All rights reserved.
 //
 
+
+//questions: to which page should it go back to after save and cancel
+//just connect cancel back to main page. (why do i need to delete, will need that when we implement a data structure)
+//check class input types
+//clicking save if nothing is entered gives an error
 import UIKit
 
-class AddCardioExercise: UIViewController {
+//making a temporary array for testing
+var tempArray1: [Exercise] = []
 
+
+class AddCardioExercise: UIViewController {
+    
+    //Mark: Properties
+    
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var cardioLabel: UILabel!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    @IBOutlet weak var exerciseNameLabel: UILabel!
+    @IBOutlet weak var exerciseNameInput: UITextField!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var speedInput: UITextField!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeInput: UITextField!
+    @IBOutlet weak var resistanceLabel: UILabel!
+    @IBOutlet weak var resistanceInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +44,51 @@ class AddCardioExercise: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+      //Mark: Action
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        
+        
+        
+        if ((exerciseNameInput.text != nil) && (timeInput.text != nil))
+        {
+            //let exerciseName1 = exerciseNameInput.text
+            //var resistance1: Int = resistanceInput.text
+            //var speed1: Int = speedInput.text
+            //var time1: Int = timeInput.text
+            
+            let newExercise = Exercise()
+            newExercise.exerciseName = exerciseNameInput.text!
+            newExercise.resistance = resistanceInput.text!
+            
+            if (speedInput.text != nil){
+            newExercise.speed = speedInput.text!
+            }
+            
+            if (timeInput.text != nil){
+            newExercise.time = timeInput.text!
+            }
+            
+            newExercise.categories = "Cardio"
+            tempArray1.append(newExercise)
+            
+            
+            //Code for testing
+            for element in tempArray1
+            {
+                element.printName()
+                element.printResistance()
+                element.printSpeed()
+                element.printTime()
+                
+            }
+            
+        }
+    }
+    
+    
+    @IBAction func cancelButton(_ sender: UIButton) {
+        
+    }
 }
 
--- AliveSFU/AliveSFU/AddStrengthExercise.swift
+++ AliveSFU/AliveSFU/AddStrengthExercise.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class AddStrengthExercise: UIViewController {
+//making a temporary array for testing
+var tempArray: [Exercise] = []
 
+
+class AddStrengthExercise: UIViewController {
+    
+    //Mark: Properties
+    
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var strengthLabel: UILabel!
+    @IBOutlet weak var exerciseNameLabel: UILabel!
+    @IBOutlet weak var exerciseNameInput: UITextField!
+    @IBOutlet weak var setsLabel: UILabel!
+   
+    @IBOutlet weak var setsInput: UITextField!
+    @IBOutlet weak var repsLabel: UILabel!
+    @IBOutlet weak var repsInput: UITextField!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +37,45 @@ class AddStrengthExercise: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Mark: Action
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+    
+        
+        //***check for valid values like max number of characters 
+        // that can be entered etc.
+        
+        // create a new object
+        
+        if (exerciseNameInput.text != nil && setsInput.text != nil && repsInput.text != nil)
+        {
+            let newExercise = Exercise()
+            
+            newExercise.exerciseName = exerciseNameInput.text!
+            newExercise.sets = setsInput.text!
+            newExercise.reps = repsInput.text!
+            
+            newExercise.categories = "Strength"
+            
+            tempArray.append(newExercise)
+            
+            
+             //Code for testing
+             for element in tempArray
+             {
+                element.printName()
+                element.printReps()
+                element.printSets()
+                
+             }
+            
+        }
+    }
 
+    @IBAction func cancelButton(_ sender: Any) {
+    }
+   
 
 }
 
