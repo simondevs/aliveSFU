cf3afd91bef587086c2d244be501eb7c1fb50176
Vivek Sharma <viveks@node-1w7jr9qnjz2ydnmq9l00vty27.ipv6.telus.net>
2016-11-05 19:42:24 -0700

    Added graph on my progress page, but it is not yet connected to actual app data. That needs to be implemented before this graph can show it, obviously. Also, maybe the graph should be adjusted to show a percentage of exercises completed for that day, instead of just exercises completed, that way, the user can have a goal of making all bars reach maximum height.

---
+++ AliveSFU/JBChartTests/JBChartTests.m
@@ -0,0 +1,39 @@
+//
+//  JBChartTests.m
+//  JBChartTests
+//
+//  Created by Vivek Sharma on 2016-11-05.
+//  Copyright © 2016 SimonDevs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JBChartTests : XCTestCase
+
+@end
+
+@implementation JBChartTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
