3825460bb4612adf6a1477968e5d499be1abc118
Gur Kohli <gkohli@sfu.ca>
2016-11-12 16:32:25 -0800

    #291970, 275084, 291974 - Added login page with button + Fixed placement of add strength and cardio

--- AliveSFU/AliveSFU/LoginPageViewController.swift
+++ AliveSFU/AliveSFU/LoginPageViewController.swift
@@ -1,25 +1,28 @@
 //
-//  AddNewExerciseController.swift
+//  LoginPageViewController.swift
 //  AliveSFU
 //
-//  Created by Gur Kohli on 2016-10-26.
+//  Created by Gur Kohli on 2016-11-12.
 //  Copyright © 2016 SimonDevs. All rights reserved.
 //
 
 import UIKit
 
-class AddNewExerciseController: UIViewController {
-
+class LoginPageViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func login(_ sender: Any) {
+    }
+    
 }
+
 
