fae1902fb4e00fed66ce22f1e113fc703d572180
Gur Kohli <gkohli@sfu.ca>
2016-11-12 16:32:25 -0800

    #291970, 275084, 291974 - Added login page with button + Fixed placement of add strength and cardio

--- AliveSFU/AliveSFU/PracticeBarGraph.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PracticeBarGraph.swift
-//  AliveSFU
-//
-//  Created by Vivek Sharma on 2016-11-05.
-//  Copyright © 2016 SimonDevs. All rights reserved.
-//
-
-import Foundation
---
+++ AliveSFU/AliveSFU/SFUCASViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SFUCASViewController.swift
+//  AliveSFU
+//
+//  Created by Gur Kohli on 2016-11-12.
+//  Copyright © 2016 SimonDevs. All rights reserved.
+//
+
+import UIKit
+
+class SFUCASViewController: UIViewController {
+    @IBOutlet weak var webContainer: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadCAS()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func loadCAS() {
+        // TODO - Talk to SFU IT to get registerd
+        
+        let url = URL(string: "https://cas.sfu.ca/cas/login?service=http://my/url")
+        let req = URLRequest(url: url!)
+        webContainer.loadRequest(req)
+    }
+    
+}
