98725407b87e84ae05e5ffba762e45179c7adb32
Gur Kohli <gkohli@sfu.ca>
2016-11-25 17:36:14 -0800

    Added SFU Recreation UI + Dropdown animation for today's hours

--- AliveSFU/AliveSFU/SFURecreationController.swift
+++ AliveSFU/AliveSFU/SFURecreationController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import JBChart
 
 class SFURecreationController: UIViewController {
 
+    
+    
+    @IBOutlet weak var graphView: JBBarChartView!
+    @IBOutlet weak var todayHours: UILabel!
+    @IBOutlet weak var overlayHours: UIView!
+    @IBOutlet weak var todayHourView: UIView!
+    @IBOutlet weak var overlayHeight: NSLayoutConstraint!
+    
+    var isOverlayOpen = false
+    var isAnimating = false
+    
+    let HEIGHT_OF_OVERLAY:CGFloat = 180
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector (todayHoursAction(_:)))
+        todayHourView.addGestureRecognizer(tapGesture)
+        todayHourView.isUserInteractionEnabled = true
+        
+        overlayHeight.constant = 0
+        isOverlayOpen = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,39 @@ class SFURecreationController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+        
+    }
 
+    func todayHoursAction(_ recognizer: UITapGestureRecognizer) {
+        if (!isOverlayOpen) {
+            //Open overlay
+            var frame = overlayHours.frame
+            frame.size.height = HEIGHT_OF_OVERLAY
+            if (!isAnimating) {
+                isAnimating = true
+                UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseInOut, animations: {self.overlayHours.frame = frame}, completion: {(completed: Bool) -> Void in
+                    self.isAnimating = false
+                    self.isOverlayOpen = true
+                })
+            }
+        } else {
+            var frame = overlayHours.frame
+            frame.size.height = 0
+            if (!isAnimating) {
+                isAnimating = true
+                
+                UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseInOut, animations: {self.overlayHours.frame = frame}, completion: {(completed: Bool) -> Void in
+                    self.isAnimating = false
+                    self.isOverlayOpen = false
+                })
+            }
+        }
+    }
 }
 
