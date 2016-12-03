6cf328cf7406adccb8604a5b5d4c1a9f6944280d
Gur Kohli <gkohli@sfu.ca>
2016-12-02 18:24:29 -0800

    Completed UI for FAQ in SFU Recreation.
    The content is empty because there's no FAQ data yet

---
+++ AliveSFU/AliveSFU/FAQViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FAQViewController.swift
+//  AliveSFU
+//
+//  Created by Gur Kohli on 2016-12-02.
+//  Copyright © 2016 SimonDevs. All rights reserved.
+//
+
+import UIKit
+
+class FAQViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        //self.navigationController?.isNavigationBarHidden = false
+        
+    }
+}
+
